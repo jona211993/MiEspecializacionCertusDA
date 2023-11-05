b99d67b81671a451a20ff9a0deb189484624b015
JONATAN PACORA VEGA <jona.jyn.27@hotmail.com>
2023-11-05 08:30:41 -0500

    agregamos Favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,25 @@
-import Banner from "../components/Banner/Banner"
-import NavBar from "../components/NavBar/NavBar"
-import Section from "../components/Section/Section"
-import Footer from '../components/Footer/Footer'
-import {useEffect } from "react";
+import Banner from "../components/Banner/Banner";
+import NavBar from "../components/NavBar/NavBar";
+import Section from "../components/Section/Section";
+import Footer from "../components/Footer/Footer";
+import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+
 function Home() {
   useEffect(() => {
     Aos.init();
   }, []);
   return (
     <>
-     <NavBar></NavBar>
-     <div data-aos="zoom-in-up" data-aos-duration="1500" >
-      <Banner></Banner>
+      <NavBar></NavBar>
+      <div data-aos="zoom-in-up" data-aos-duration="1500">
+        <Banner></Banner>
       </div>
-     <Section></Section>
-     <Footer></Footer>
+      <Section></Section>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/PageModulo1.jsx
+++ src/pages/PageModulo1.jsx
@@ -1,13 +1,12 @@
 import { useRef, useEffect } from "react";
+import NavBar from "../components/NavBar/NavBar";
+import CardImg from "../components/CardImg/CardImg";
+import MyFooter from "../components/Footer/Footer";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import lottie from "lottie-web";
-import { Image } from "antd";
-import NavBar from "../components/NavBar/NavBar";
-import CardImg from "../components/CardImg/CardImg";
 import noteData from "../assets/lotties/dbanimacion.json";
 import "./styles/pageMod1.css";
-import MyFooter from "../components/Footer/Footer";
 
 function PageModulo1() {
   const container = useRef(null);
@@ -28,27 +27,34 @@ function PageModulo1() {
     <>
       <NavBar></NavBar>
       <div className="cont-wraper">
-        <div data-aos="flip-up" data-aos-duration="2000" className="cont-titulo">
+        <div
+          data-aos="flip-up"
+          data-aos-duration="2000"
+          className="cont-titulo"
+        >
           <h1>Base de Datos con MS SQL Server 2019</h1>
         </div>
         <h2>Tema 1: SQL: Modelamiento de base de datos</h2>
         <div className="cont-intro-tema">
-          <div className=" cont-texto"
-          >
-          <p>
-            Al finalizar esta sesión, conocí la importancia de una base de datos
-            en una organización y la arquitectura de la misma. Comprendí la
-            importancia de diagramar el modelo lógico y físico dentro de la
-            organización, así como definir las diferencias de cada modelo . Para
-            hacer los diagramas usamos la herramienta Erwin.
-          </p>
+          <div className=" cont-texto">
+            <p>
+              Al finalizar esta sesión, conocí la importancia de una base de
+              datos en una organización y la arquitectura de la misma. Comprendí
+              la importancia de diagramar el modelo lógico y físico dentro de la
+              organización, así como definir las diferencias de cada modelo .
+              Para hacer los diagramas usamos la herramienta Erwin.
+            </p>
           </div>
 
           <div ref={container} className="cont-img-animada"></div>
         </div>
         <h3>Ejercicios Prácticos de Clase - Modelo Lógico</h3>
         <p>Modelar los siguientes casos:</p>
-        <div className="cont-cardImg" data-aos="fade-right" data-aos-duration="1500">
+        <div
+          className="cont-cardImg"
+          data-aos="fade-right"
+          data-aos-duration="1500"
+        >
           <CardImg
             title="Caso 1: Caso de Reserva de Vehículos"
             ancho={300}
@@ -68,7 +74,11 @@ function PageModulo1() {
         <br />
         <h3>Ejercicios de Clase - Normalización y Modelo Físico </h3>
         <h4>Factura de Compra</h4>
-        <div className="cont-cardImg" data-aos="fade-left" data-aos-duration="1500">
+        <div
+          className="cont-cardImg"
+          data-aos="fade-left"
+          data-aos-duration="1500"
+        >
           <CardImg
             title="Factura"
             ancho={300}
@@ -85,9 +95,13 @@ function PageModulo1() {
             url_img={"https://i.postimg.cc/TPcj2CNx/modelofisico-fc.jpg"}
           ></CardImg>
         </div>
-        
+
         <h4>Ticket de Compra</h4>
-        <div className="cont-cardImg" data-aos="fade-right" data-aos-duration="1500">
+        <div
+          className="cont-cardImg"
+          data-aos="fade-right"
+          data-aos-duration="1500"
+        >
           <CardImg
             title="Ticket"
             ancho={300}
@@ -104,7 +118,6 @@ function PageModulo1() {
             url_img={"https://i.postimg.cc/QCsWSGvy/modfis2.jpg"}
           ></CardImg>
         </div>
-       
       </div>
       <MyFooter></MyFooter>
     </>
